31b25369469a479a1918046e84a397f4842bebb4
min33sky <min33sky@naver.com>
2022-11-03 02:48:56 +0900

    feat: success page added

---
+++ client/src/pages/cancel.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Cancel() {
+  return <div className="text-3xl font-bold text-green-500">결제 실패...</div>;
+}
---
+++ client/src/pages/success.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Success() {
+  return (
+    <div className="grid h-full place-items-center bg-slate-200 text-3xl font-bold text-green-700">
+      결제 성공!!
+    </div>
+  );
+}
